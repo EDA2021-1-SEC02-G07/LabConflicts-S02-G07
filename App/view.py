db72585f36f5b1e85ab20d4130a9a91e3c4240b7
Mario Ricaurte <m.ricaurte@uniandes.edu.co>
2021-02-10 19:38:09 -0500

    Entrega Final – laboratorio 2

--- App/view.py
+++ App/view.py
@@ -25,7 +25,7 @@ def loadBooks():
     """
     Carga los libros
     """
-    return controller.loadBooks('GoodReads/books-small.csv')
+    return controller.loadBooks('GoodReads/books.csv')
 
 
 def loadTags():
@@ -38,7 +38,7 @@ def loadBookTags():
     """
     Cargar los Tags de libros
     """
-    return controller.loadBooks('GoodReads/book_tags-small.csv')
+    return controller.loadBooks('GoodReads/book_tags.csv')
 
 
 """
